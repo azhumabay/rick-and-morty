130e2901e9c77d2831d188f8626200782515ece8
azhumabay <zhuma.2001ali@gmail.com>
2024-10-27 05:21:29 +0500

    addedd alias

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import router from "./router/router.jsx";
-import GlobalStyles from "./components/styled/Global.js";
+import GlobalStyles from "@components/styled/Global.js";
 import useThemeStore from "./store/useThemeStore.js";
 import { darkTheme, lightTheme } from "./const/theme.js";
 
